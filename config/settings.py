72c1e10d9877fb22ca3cdf8ce81a162ff667de90
Exulten <stem.danieljohnbucad@gmail.com>
2025-10-29 21:59:01 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -139,15 +139,7 @@ else:
 FIREBASE_API_KEY = config('FIREBASE_API_KEY', default='')
 FIREBASE_AUTH_DOMAIN = config('FIREBASE_AUTH_DOMAIN', default='')
 FIREBASE_PROJECT_ID = config('FIREBASE_PROJECT_ID', default='')
-FIREBASE_DATABASE_URL = config('FIREBASE_DATABASE_URL', default='https://cit306-finalproject-default-rtdb.firebaseio.com/')
-
-# Celery Configuration
-CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND', default='redis://localhost:6379/0')
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = TIME_ZONE
+FIREBASE_DATABASE_URL = config('FIREBASE_DATABASE_URL', default='https://cit306-finalproject-default-rtdb.firebaseio.com/'
 
 # Logging
 LOGGING = {
